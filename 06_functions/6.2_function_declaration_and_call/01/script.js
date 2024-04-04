256c7ebfa4848f6deeda0dd51e0541f5d2b2d6d5
sergeybabann <sergeybabann@yandex.ru>
2024-04-04 23:43:12 +0300

    additions

---
+++ 06_functions/6.2_function_declaration_and_call/01/script.js
@@ -0,0 +1,6 @@
+function myFn(a, b) {
+    let c
+    a = a + 1
+    c = a + b
+    return c
+}
---
+++ 06_functions/6.2_function_declaration_and_call/02/script.js
@@ -0,0 +1,9 @@
+function myFn(a, b) {
+    let c
+    a = a + 1
+    c = a + b
+    return c
+}
+
+myFn(10, 3) // 14
+
